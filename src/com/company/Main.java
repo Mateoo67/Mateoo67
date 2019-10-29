2e77b35254cb6c229e5287466a240208d6ed5427
Mateoo67 <57131850+Mateoo67@users.noreply.github.com>
2019-10-29 10:35:47 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Halo");
+	    System.out.println("Hello new user");
+		
     }
 }
